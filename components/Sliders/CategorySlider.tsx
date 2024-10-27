3b56486010128ba063d847e66e696a737d45326c
Piyush <piyushtiwarindls220@gmail.com>
2024-10-27 22:06:00 +0300

    fixed category slider and added jpgs for category

--- components/Sliders/CategorySlider.tsx
+++ components/Sliders/CategorySlider.tsx
@@ -7,36 +7,73 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
+const breakpoints = {
+  320: {
+    slidesPerView: 3,
+  },
+  640: {
+    slidesPerView: 4,
+  },
+  768: {
+    slidesPerView: 6,
+  },
+};
+
+interface gameCategory {
+  slug: string;
+  image: string;
+  title: string;
+  description?: string;
+}
+
 export default function CategorySlider({
   gameCategories,
 }: {
-  gameCategories: any;
+  gameCategories: gameCategory[];
 }) {
+  console.log(gameCategories);
   return (
-    <div>
-      <div className="flex justify-between">
-        <h2>{gameCategories.title}</h2>
-        <a href={`/category/${gameCategories.slug}`}>
-          View All <ChevronRightIcon className="w-4 h-4" />
+    <div className="mb-6">
+      <div className="flex justify-between gap-4">
+        <h2 className="font-sans mb-4">Categories</h2>
+        <a
+          href={`/category`}
+          className="text-sm font-medium hover:underline underline-offset-4"
+        >
+          View All{" "}
+          <ChevronRightIcon className="w-4 h-4 text-accent inline-block" />
         </a>
       </div>
       <Swiper
         modules={[Navigation, Scrollbar, A11y]}
         spaceBetween={20}
         slidesPerView={6}
+        breakpoints={breakpoints}
         navigation
         scrollbar={{ draggable: true }}
+        style={
+          {
+            "--swiper-pagination-color": "#FFBA08",
+            "--swiper-pagination-bullet-incactive-color": "#999999",
+            "--swiper-pagination-bullet-incactive-opacity": "1",
+            "--swiper-pagination-bullet-size": "0.6em",
+            "--swiper-pagination-bullet-horizontal-gap": "6px",
+            "--swiper-theme-color": "#FFF",
+            "--swiper-navigation-size": "24px",
+            "--swiper-navigation-sides-offset": "30px",
+          } as React.CSSProperties
+        }
       >
         {gameCategories.map((item, i) => (
-          <SwiperSlide key={i}>
+          <SwiperSlide key={i} className="group">
             <a href={`/category/${item.slug}`} className="group">
-              <div>
+              <div className="overflow-hidden rounded-lg border border-accentSecondary mb-2">
                 <Image
                   src={`/category/${item.image}`}
                   width={220}
                   height={320}
                   alt={item.title}
-                  className="w-full h-full"
+                  className="w-full h-full transition-transform duration-300 group-hover:scale-105"
                 />
               </div>
               <h1>{item.title}</h1>
--- components/Sliders/HeroSlider.tsx
+++ components/Sliders/HeroSlider.tsx
@@ -16,48 +16,64 @@ export default function HeroSlider() {
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         className="h-[340px] md:h-[480px] bg-main rounded-lg border border-accentSecondary mb-6"
-        style={{
-          "--swiper-pagination-color": "#ffba08",
-          "--swiper-pagination-bullet-inactive-color": "#999999",
-          "--swiper-pagination-bullet-inactive-opacity": "1",
-          "--swiper-pagination-bullet-size": "0.6em",
-          "--swiper-pagination-bullet-horizontal-gap": "6px",
-          "--swiper-theme-color": "#fff",
-          "--swiper-navigation-size": "24px",
-          "--swiper-navigation-sides-offset": "30px",
-        }}
+        style={
+          {
+            "--swiper-pagination-color": "#ffba08",
+            "--swiper-pagination-bullet-inactive-color": "#999999",
+            "--swiper-pagination-bullet-inactive-opacity": "1",
+            "--swiper-pagination-bullet-size": "0.6em",
+            "--swiper-pagination-bullet-horizontal-gap": "6px",
+            "--swiper-theme-color": "#fff",
+            "--swiper-navigation-size": "24px",
+            "--swiper-navigation-sides-offset": "30px",
+          } as React.CSSProperties
+        }
       >
-        <SwiperSlide className="bg-[url('/slide/slide-1.png')] bg-no-repeat bg-right p-20 items-center" style={{
-          display:"flex"
-        }}>
+        <SwiperSlide
+          className="bg-[url('/slide/slide-1.png')] bg-no-repeat bg-center md:bg-right px-16 md:p-20 items-center"
+          style={{
+            display: "flex",
+          }}
+        >
           <div className="max-w-3xl">
             <div className="text-accent text-sm mb-2 uppercase">
               free arcade games
             </div>
-            <h1 className="text-4xl lg:text-6xl mb-4 font-sans font-bold">
+            <h1 className="text-2xl md:text-4xl lg:text-6xl mb-4 font-sans font-bold">
               PLAY RETRO GAMES FOR FREE
             </h1>
             <p className="max-w-[418px] mb-6">
               Relive the Classics! Dive into our Collection of retro games and
               enjoy them for free. Start playing for Now!
             </p>
-            <a href="/" className="text-sm bg-accent-gradient px-6 py-3 rounded-xl border border-yellow-400 uppercase hover:opacity-70">Play Unfair Mario</a>
+            <a
+              href="/"
+              className="text-sm bg-accent-gradient px-6 py-3 rounded-xl border border-yellow-400 uppercase hover:opacity-70"
+            >
+              Play Unfair Mario
+            </a>
           </div>
         </SwiperSlide>
-        <SwiperSlide className="bg-[url('/slide/slide-1.png')] bg-no-repeat bg-right p-20 items-center" style={{
-          display:"flex"
-        }}>
+        <SwiperSlide
+          className="bg-[url('/slide/slide-1.png')] bg-no-repeat bg-right p-20 items-center"
+          style={{
+            display: "flex",
+          }}
+        >
           <div className="max-w-3xl">
             <div className="text-accent text-sm mb-2 uppercase">
               free arcade games
             </div>
             <h1 className="text-4xl lg:text-6xl mb-4 font-sans font-bold">
-             #2
+              #2
             </h1>
-            <p className="max-w-[418px] mb-6">
-              play something
-            </p>
-            <a href="/" className="text-sm bg-accent-gradient px-6 py-3 rounded-xl border border-yellow-400 uppercase hover:opacity-70">Play Unfair Mario</a>
+            <p className="max-w-[418px] mb-6">play something</p>
+            <a
+              href="/"
+              className="text-sm bg-accent-gradient px-6 py-3 rounded-xl border border-yellow-400 uppercase hover:opacity-70"
+            >
+              Play Unfair Mario
+            </a>
           </div>
         </SwiperSlide>
       </Swiper>
